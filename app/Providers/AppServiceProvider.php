d37731ee4b37c01f50dab416afa1d86f9df7d185
Attila Fulop <fulopattila122@gmail.com>
2018-05-12 23:50:14 +0200

    Bumped modules to v0.3-dev, PHP to 7.1.3+

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,7 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Schema;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -13,6 +14,8 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
+        Schema::defaultStringLength(191);
+
         $this->app->concord->registerModel(\Konekt\User\Contracts\User::class, \App\User::class);
     }
 
