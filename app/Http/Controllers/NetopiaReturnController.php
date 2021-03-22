05117d70fec883c89446cdc6588c53d95b7eb012
Attila Fulop <1162360+fulopattila122@users.noreply.github.com>
2021-03-22 17:27:04 +0100

    Added payment history and latest Netopia plugin updates (1.1-dev)

--- app/Http/Controllers/NetopiaReturnController.php
+++ app/Http/Controllers/NetopiaReturnController.php
@@ -10,6 +10,7 @@ use Vanilo\Payment\Events\PaymentCompleted;
 use Vanilo\Payment\Events\PaymentDeclined;
 use Vanilo\Payment\Events\PaymentPartiallyReceived;
 use Vanilo\Payment\Models\Payment;
+use Vanilo\Payment\Models\PaymentHistory;
 use Vanilo\Payment\Models\PaymentStatus;
 use Vanilo\Payment\PaymentGateways;
 
@@ -37,25 +38,17 @@ class NetopiaReturnController extends Controller
             return new ErrorResponseToNetopia(404, 'Could not locate payment with id ' . $response->getPaymentId());
         }
 
-        if ($response->wasSuccessful()) {
-            $payment->amount_paid = $response->getAmountPaid();
-            if ($response->getAmountPaid() < $payment->getAmount()) {
-                $payment->status = PaymentStatus::PARTIALLY_PAID();
-                $payment->status_message = $response->getMessage();
-                $payment->save();
-                event(new PaymentPartiallyReceived($payment, $response->getAmountPaid()));
-            } else {
-                $payment->status = PaymentStatus::PAID();
-                $payment->status_message = $response->getMessage();
-                $payment->save();
-                event(new PaymentCompleted($payment));
-            }
-        } else {
-            $payment->status = PaymentStatus::DECLINED();
-            $payment->status_message = $response->getMessage();
-            $payment->save();
-            event(new PaymentDeclined($payment));
-        }
+        PaymentHistory::addPaymentResponse($payment, $response);
+
+        $payment->status = $response->getStatus();
+        $payment->amount_paid = $response->getAmountPaid();
+        $payment->status_message = $response->getMessage();
+        $payment->save();
+
+        // Missing: firing events:
+//                event(new PaymentPartiallyReceived($payment, $response->getAmountPaid()));
+//                event(new PaymentCompleted($payment));
+//            event(new PaymentDeclined($payment));
 
         return new SuccessResponseToNetopia();
     }
