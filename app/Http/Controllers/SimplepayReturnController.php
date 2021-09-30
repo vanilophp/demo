37c88006c86f5c9302f89fa4fc7087ae8e93baa7
Hunor Kedves <hunor@artkonekt.com>
2021-09-30 14:04:35 +0300

    Converted Simplepay return controller to use the PaymentResponseHandler

--- app/Http/Controllers/SimplepayReturnController.php
+++ app/Http/Controllers/SimplepayReturnController.php
@@ -5,12 +5,9 @@ namespace App\Http\Controllers;
 use Illuminate\Database\Eloquent\ModelNotFoundException;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Log;
-use Vanilo\Payment\Events\PaymentCompleted;
-use Vanilo\Payment\Events\PaymentDeclined;
-use Vanilo\Payment\Events\PaymentPartiallyReceived;
 use Vanilo\Payment\Models\Payment;
-use Vanilo\Payment\Models\PaymentStatus;
 use Vanilo\Payment\PaymentGateways;
+use Vanilo\Payment\Processing\PaymentResponseHandler;
 
 class SimplepayReturnController extends Controller
 {
@@ -19,36 +16,20 @@ class SimplepayReturnController extends Controller
         Log::debug('SimplePay confirmation', $request->toArray());
 
         $response = PaymentGateways::make('simplepay')->processPaymentResponse($request);
-        $payment  = Payment::findByPaymentId($response->getPaymentId());
+        $payment = Payment::findByPaymentId($response->getPaymentId());
 
         if (!$payment) {
             return new ModelNotFoundException('Could not locate payment with id ' . $response->getPaymentId());
         }
 
-        if ($response->wasSuccessful()) {
-            $payment->amount_paid = $response->getAmountPaid();
-
-            if ($response->getAmountPaid() < $payment->getAmount()) {
-                $payment->status         = PaymentStatus::PARTIALLY_PAID();
-                $payment->status_message = $response->getMessage();
-                $payment->save();
-                event(new PaymentPartiallyReceived($payment, $response->getAmountPaid()));
-            } else {
-                $payment->status         = PaymentStatus::PAID();
-                $payment->status_message = $response->getMessage();
-                $payment->save();
-                event(new PaymentCompleted($payment));
-            }
-        } else {
-            $payment->status         = PaymentStatus::DECLINED();
-            $payment->status_message = $response->getMessage();
-            $payment->save();
-            event(new PaymentDeclined($payment));
-        }
+        $handler = new PaymentResponseHandler($payment, $response);
+        $handler->writeResponseToHistory();
+        $handler->updatePayment();
+        $handler->fireEvents();
 
         return view('payment.return', [
-            'payment'  => $payment,
-            'order'    => $payment->getPayable()
+            'payment' => $payment,
+            'order' => $payment->getPayable(),
         ]);
     }
 }
